3239dc0982d7d5410e57c7f56e1b0bdffeec04c1
Samuel barbosa almeida <barbosasamuel24@gmail.com>
2022-10-06 15:08:34 -0300

    update

--- Front-app/weight-tracker-app/src/App.vue
+++ Front-app/weight-tracker-app/src/App.vue
@@ -1,3 +1,28 @@
+<script>
+import { getAuth } from "firebase/auth";
+
+const auth = getAuth();
+
+export default{
+	data() {
+		return {
+			email: auth.currentUser.email,
+		}
+	},
+	methods: {
+		signOut() {
+			auth
+					.signOut()
+					.then(() => {
+						console.log("Sign Out completed");
+						this.$router.push("/");
+					})
+					.catch((error) => console.log(error) )
+		}
+	}
+}
+</script>
+
 <template>
   <div class="background"></div>
   <div class="navbar">
@@ -11,6 +36,12 @@
           >GitHub Repo</a
         >
       </div>
+
+      <div class="text-center">You're now logged as</div>
+    <div id="username_display" class="display-6">{{ this.email }}</div>
+    <button id="sign_out" class="mt-4 btn btn-danger" @click="signOut">
+      Logout
+    </button>
     </nav>
   </div>
   <router-view id="content" />
